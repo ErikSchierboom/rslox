1d194b4b481823c19523f2643393cb984c0b8707
Erik Schierboom <erik_schierboom@hotmail.com>
2024-08-11 15:00:35 +0200

    Start with scanner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+mod scanner;
+
+use std::any::Any;
+use std::env::args;
+use std::fs;
+use std::io::stdin;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: convert to match
+    if args().len() > 1 {
+        eprintln!("Usage: rslox [script]");
+        exit(64);
+    } else if args().len() == 1 {
+        run_file(args().nth(1).unwrap())
+    } else {
+        run_prompt()
+    }
 }
+
+fn run_file(path: String) {
+    match fs::read_to_string(path) {
+        Ok(source) => run(source),
+        Err(e) => {
+            eprintln!("Error reading file: {}", e);
+            exit(74);
+        }
+    }
+}
+
+fn run_prompt() {
+    let mut input = String::new();
+    loop {
+        println!("> ");
+        match stdin().read_line(&mut input) {
+            Ok(_) => run(input.clone()),
+            Err(e) => {
+                eprintln!("Error reading input: {}", e);
+                exit(74);
+            }
+        }
+    }
+}
+
+fn run(source: String) {
+    let mut scanner = scanner::Scanner::new(source);
+    let tokens = scanner.scan_tokens();
+
+    for token in tokens {
+        println!("{:?}", token);
+    }
+}
+
---
+++ src/scanner.rs
@@ -0,0 +1,99 @@
+#[derive(Debug, Clone)]
+pub enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS,
+    LessEqual,
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
+
+#[derive(Debug, Clone)]
+pub struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    line: usize
+}
+
+pub(crate) struct Scanner {
+    source: String,
+    tokens: Vec<Token>,
+    start: usize,
+    current: usize,
+    line: usize,
+}
+
+impl Scanner {
+    pub fn new(source: String) -> Scanner {
+        Scanner {
+            source,
+            tokens: Vec::new(),
+            start: 0,
+            current: 0,
+            line: 1,
+        }
+    }
+
+    pub fn scan_tokens(&mut self) -> Vec<Token> {
+        while !self.is_at_end() {
+            self.start = self.current;
+            self.scan_token();
+        }
+
+        self.tokens.push(Token {
+            token_type: TokenType::EOF,
+            lexeme: "".to_string(),
+            line: self.line
+        });
+
+        self.tokens.clone()
+    }
+
+    fn scan_token(&mut self) {
+        let c = self.advance();
+        match c {
+            '(' => self.add_token(TokenType::LEFT_PAREN),
+            ')' => self.add_token(TokenType::RIGHT_PAREN),
+            '{' => self.add_token(TokenType::LEFT_BRACE),
+            '}' => self.add_token(TokenType::RIGHT_BRACE),
+            ',' => self.add_token(TokenType::COMMA),
+            '.' => self.add_token(TokenType::DOT),
+            '-' => self.add_token(TokenType::MINUS),
+            '+' => self.add_token(TokenType::PLUS),
+            ';' => self.add_token(TokenType::SEMICOLON),
+            '*' => self.add_token(TokenType::STAR),
+            _ => {}
+        }
+    }
+
+    pub fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
+
+    fn advance(&mut self) -> char {
+        self.current += 1;
+        self.source.chars().nth(self.current - 1).unwrap()
+    }
+
+    fn add_token(&mut self, token_type: TokenType) {
+        let text = self.source[self.start..self.current].to_string();
+        self.tokens.push(Token {
+            token_type,
+            lexeme: text,
+            line: self.line
+        });
+    }
+}
